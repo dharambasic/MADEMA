d84988c11ecfd019d7ce9b6e62abeb6829b816dc
Denis Harambasic <harambasicdenis@gmail.com>
2020-05-13 14:33:09 +0200

    d
    dd

---
+++ Assets/Scripts/KameraKontroler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontroler : MonoBehaviour
+{
+
+
+    public GameObject player;
+
+    private Vector3 pomak;
+
+    void Start()
+    {
+        pomak = transform.position - player.transform.position;    
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + pomak;
+    }
+}
---
+++ Assets/Scripts/KontrolaIgre.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KontrolaIgre : MonoBehaviour
+{
+
+    public float zSila = 0;
+
+   
+    void Start()
+    {
+       
+    }
+
+   
+    void Update()
+    {
+
+        if(Input.GetKeyDown("z"))
+        {
+            zSila += 100;
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            zSila += 10;
+        }
+
+        
+    }
+    
+
+   void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(0, 0, zSila);
+    }
+}
